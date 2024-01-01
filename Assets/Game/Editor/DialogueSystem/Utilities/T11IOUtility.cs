6a7a8900f60bedc061bf6f680bde80bb3604138d
test11 <kaantekin67@gmail.com>
2024-01-01 21:36:47 +0300

    project edit

---
+++ Assets/Game/Editor/DialogueSystem/Utilities/T11IOUtility.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace T11.Utilities
+{
+    public static class T11IOUtility
+    {
+        public static void Save()
+        {
+
+        }
+    }
+}
